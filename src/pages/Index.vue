150a37af658525de2470af62a1796ca70992f4ab
Divyanshi Agarwal <divyanshi@ip-10-10-10-11.ap-south-1.compute.internal>
2021-06-24 14:52:17 +0530

    Configured gridsome vue-remark

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Helloooo</h1>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
